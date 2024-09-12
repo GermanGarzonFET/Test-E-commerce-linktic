b858bcff0c58bb90de5ebcadb8a3d9b9e7441f32
German Garzon <german_garzonto@fet.edu.co>
2024-09-11 19:23:11 -0500

    Front y documentacion.
    Front y documentacion.

--- Documentacion/db_ecommerce.sql
+++ Documentacion/db_ecommerce.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 11-09-2024 a las 22:33:11
+-- Tiempo de generación: 12-09-2024 a las 02:17:12
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -41,7 +41,12 @@ CREATE TABLE `detalle_pedidos` (
 --
 
 INSERT INTO `detalle_pedidos` (`id`, `cantidad`, `precio_unidad`, `subtotal`, `orden_id`, `producto_id`) VALUES
-(1, 2, 5, 10, 1, 1);
+(3, 4, 350.99, 1403.96, 3, 1),
+(4, 5, 350.99, 1754.95, 4, 1),
+(5, 10, 350.99, 3509.9, 4, 2),
+(6, 1, 350.99, 350.99, 5, 1),
+(7, 1, 350.99, 350.99, 6, 1),
+(8, 4, 350.99, 1403.96, 6, 2);
 
 -- --------------------------------------------------------
 
@@ -60,7 +65,10 @@ CREATE TABLE `ordenes` (
 --
 
 INSERT INTO `ordenes` (`id`, `fecha`, `total`) VALUES
-(1, '2024-09-25 15:00:29.000000', 50000);
+(3, '2024-09-11 17:14:38.000000', 1403.96),
+(4, '2024-09-11 18:05:13.000000', 5264.85),
+(5, '2024-09-11 19:09:36.000000', 350.99),
+(6, '2024-09-11 19:13:48.000000', 1754.95);
 
 -- --------------------------------------------------------
 
@@ -82,8 +90,13 @@ CREATE TABLE `productos` (
 --
 
 INSERT INTO `productos` (`id`, `descripcion`, `imagen`, `nombre`, `precio`, `stock`) VALUES
-(1, 'Cámara de alta resolución con lente intercambiable.', 'https://example.com/imagen1.jpg', 'Cámara Digital eeedite', 350.99, 15),
-(2, 'Cámara de alta resolución con lente intercambiable.', 'https://example.com/imagen1.jpg', 'Cámara Digital', 350.99, 15);
+(1, 'Cámara de alta resolución con lente intercambiable.', 'https://example.com/imagen1.jpg', 'Cámara Digital eeedite', 350.99, 6),
+(2, 'Cámara de alta resolución con lente intercambiable.', 'https://example.com/imagen1.jpg', 'Cámara Digital', 350.99, 1),
+(3, 'Auriculares inalámbricos con cancelación de ruido.', 'https://example.com/imagen2.jpg', 'Auriculares Bluetooth', 79.99, 25),
+(4, 'Laptop ligera y potente con pantalla Full HD.', 'https://example.com/imagen3.jpg', 'Laptop Ultrabook', 1200.5, 10),
+(5, 'Mouse ergonómico inalámbrico con batería recargable.', 'https://example.com/imagen10.jpg', 'Mouse Inalámbrico', 49.99, 60),
+(6, 'Consola de última generación con soporte para juegos 4K.', 'https://example.com/imagen9.jpg', 'Consola de Videojuegos', 499.99, 8),
+(7, 'Monitor de alta resolución 4K con tecnología IPS.', 'https://example.com/imagen8.jpg', 'Monitor 4K', 599.99, 12);
 
 -- --------------------------------------------------------
 
@@ -105,7 +118,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `email`, `firstname`, `lastname`, `password`, `username`) VALUES
-(1, 'correo@correo.com', 'iva', 'rojas', '$2a$10$lz6hifQqhY8nxHWoXSTsLu3UZkVC/xsytVyTHEZ9.5Z2VqOdZvxJ.', 'user2024');
+(1, 'correo@correo.com', 'iva', 'rojas', '$2a$10$lz6hifQqhY8nxHWoXSTsLu3UZkVC/xsytVyTHEZ9.5Z2VqOdZvxJ.', 'user2024'),
+(2, 'correo@correo.com', 'iva', 'rojas', '$2a$10$.P1rdlSGG/9O4peoZ4a3recEnDLOBBHEmrtGfWplvtSeT2ydHSfuS', 'admin');
 
 --
 -- Índices para tablas volcadas
@@ -145,25 +159,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `detalle_pedidos`
 --
 ALTER TABLE `detalle_pedidos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `ordenes`
 --
 ALTER TABLE `ordenes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Restricciones para tablas volcadas
